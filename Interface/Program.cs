a76ca340bc4f8307441d1a7452eca1ea577d6158
arminedaniel999@gmail.com <arminedaniel999@gmail.com>
2020-04-26 18:27:14 +0400

    //

---
+++ Interface/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface IMovable
+    {
+        public const int minSpeed = 0;
+        private static int maxSpeed = 60;
+        static double GetTime(double distance, double speed) => distance / speed;
+        static int MaxSpeed
+        {
+            get { return maxSpeed; }
+            set
+            {
+                if (value > 0) maxSpeed = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IMovable.MaxSpeed);
+            IMovable.MaxSpeed = 65;
+            Console.WriteLine(IMovable.MaxSpeed);
+            double time = IMovable.GetTime(100, 10);
+            Console.WriteLine(time);
+            Console.Read();
+        }
+    }
+}
+
---
+++ Interface1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Interface1
+{
+    interface IMovable
+    {
+        void Move();
+    }
+    class Person : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("The person goes");
+        }
+    }
+    struct Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("The car goes");
+        }
+    }
+    class Program
+    {
+        static void Action(IMovable movable)
+        {
+            movable.Move();
+        }
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Car car = new Car();
+            Action(person);
+            Action(car);
+            Console.Read();
+        }
+    }
+}
